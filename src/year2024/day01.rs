77a3a10d12ed276e8ba610279a5cb68f3f101373
Nicholas Thompson <njthompson1004@gmail.com>
2025-11-26 10:47:22 +0000

    2024 day 1 part 1

--- src/year2024/day01.rs
+++ src/year2024/day01.rs
@@ -15,7 +15,25 @@ fn parse(contents: &str) -> Vec<(u32, u32)> {
 }
 
 fn part1(data: &Vec<(u32, u32)>) -> u32 {
-    1
+    println!("{:?}", data);
+
+    let (mut left, mut right): (Vec<u32>, Vec<u32>) = data.iter().cloned().unzip();
+
+    left.sort();
+    right.sort();
+
+    println!("{:?}", left);
+    println!("{:?}", right);
+
+    let diff: Vec<u32> = left
+        .into_iter()
+        .zip(right.into_iter())
+        .map(|(a, b)| a.abs_diff(b))
+        .collect();
+
+    println!("{:?}", diff);
+
+    11
 }
 
 fn part2() -> u32 {
@@ -23,7 +41,7 @@ fn part2() -> u32 {
 }
 
 pub fn run() -> () {
-    let path = Path::new("~/repo/inputs/2024/day01.txt");
+    let path = Path::new("/home/ubuntu/repo/inputs/2024/day01.txt");
     let contents = fs::read_to_string(path).unwrap();
 
     let data = parse(&contents);
@@ -39,19 +57,27 @@ pub fn run() -> () {
 mod tests {
 
     use super::*;
-
-    #[test]
-    fn test_parse() {
-        let contents = "
+    const CONTENTS: &str = "
             3   4
             4   3
             2   5
             1   3
             3   9
             3   3";
-        let data = parse(&contents);
+
+    #[test]
+    fn test_parse() {
+        let data = parse(&CONTENTS);
         let expected = vec![(3, 4), (4, 3), (2, 5), (1, 3), (3, 9), (3, 3)];
 
         assert_eq!(data, expected);
+    }
+
+    #[test]
+    fn test_part1() {
+        let data = parse(&CONTENTS);
+        let result = part1(&data);
+
+        assert_eq!(result, 11);
     }
 }
